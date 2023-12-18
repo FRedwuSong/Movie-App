bc2f0b4dbd174cede1e7b9da8fa790e957293e6c
FRedwuSong <diadora1166@gmail.com>
2023-12-18 11:00:47 +0800

    新增驗證

--- obj/Debug/net6.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net6.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70fcb857ef0121279c4c5e8d425ad30fa0fb1d9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
